975629d16a667aa1350054341251ec2bee804324
PatiKoszi <patrykkosznik@gmail.com>
2018-04-11 23:50:39 +0200

    Modulo i party mode od lewej do prawej zaznacza na czerwono

--- training.js
+++ training.js
@@ -14,10 +14,10 @@ function createTable() {
     table.appendChild(tbody);
 
 
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 10; i++) {
         var tr = document.createElement('tr');
         tbody.appendChild(tr);
-        for (var j = 0; j < 5; j++) {
+        for (var j = 0; j < 20; j++) {
             var td = document.createElement('td');
             tr.appendChild(td)
         }
